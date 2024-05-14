fd234dd3e7c56638c747253cfe03b8ece48aed3e
Michael Barry <BarryMichaelR@outlook.com>
2024-05-13 22:37:51 -0700

    Fixed improper var

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var Color = require('cesium/Core/Cesium');
+var Cesium = require('cesium/Cesium');
 require('./css/main.css');
 require('cesium/Widgets/widgets.css');
 var viewer = new Cesium.Viewer('cesiumContainer');
