123ff74e829f3ae6af58fa7871b577a97ce3c4b3
Michael Barry <BarryMichaelR@outlook.com>
2024-05-13 22:44:55 -0700

    Removed UI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 var Cesium = require('cesium/Cesium');
 require('./css/main.css');
 require('cesium/Widgets/widgets.css');
-var viewer = new Cesium.Viewer('cesiumContainer');
+
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    animation: false,  // Disable animation widget
+    baseLayerPicker: false,  // Disable base layer picker
+    fullscreenButton: false,  // Disable fullscreen button
+    geocoder: false,  // Disable geocoder/search widget
+    homeButton: false,  // Disable home button
+    infoBox: false,  // Disable info box
+    sceneModePicker: false,  // Disable scene mode picker
+    selectionIndicator: false,  // Disable selection indicator
+    timeline: false,  // Disable timeline widget
+    navigationHelpButton: false,  // Disable navigation help button
+    scene3DOnly: true  // Enable 3D only mode
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
-// The path to the CesiumJS source code
-const cesiumSource = 'node_modules/cesium/Source';
-const cesiumWorkers = '../Build/Cesium/Workers';
-
 const path = require('path');
-const CopywebpackPlugin = require('copy-webpack-plugin');
-const Cesium = require('cesium');
-const isProduction = process.env.NODE_ENV === 'production';
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     context: __dirname,
@@ -19,29 +16,25 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
-
-        // Needed to compile multiline strings in Cesium
         sourcePrefix: ''
     },
     amd: {
-        // Enable webpack-friendly use of require in Cesium
         toUrlUndefined: true
     },
     module: {
         rules: [{
             test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
+            use: ['style-loader', 'css-loader']
         }, {
             test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-            use: [ 'url-loader' ]
+            use: ['url-loader']
         }]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html'
         }),
-        // Copy Cesium Assets, Widgets, and Workers to a static directory
-        new CopywebpackPlugin({ 
+        new CopyWebpackPlugin({ 
             patterns: [
                 { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
                 { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
@@ -49,18 +42,16 @@ module.exports = {
             ]
         }),
         new webpack.DefinePlugin({
-            // Define relative base path in cesium for loading assets
             CESIUM_BASE_URL: JSON.stringify('')
         })
     ],
     devServer: {
         static: {
             directory: path.join(__dirname, 'dist'),
-          },
+        },
     },
     resolve: {
         alias: {
-            // CesiumJS module name
             cesium: path.resolve(__dirname, cesiumSource)
         }
     },
